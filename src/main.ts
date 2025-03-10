4ec1622bf620ffc8ed19c32465fa83f44fa92e44
coolAbdo <abdelrahman.egschool@gmail.com>
2025-03-11 01:58:01 +0200

    added mongo connection and resolved some dependencies

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppConfig } from './config/app.config';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+  app.use(cookieParser());
   const config = new DocumentBuilder()
     .setTitle('API Documentation')
     .setDescription('The API documentation for the project')
