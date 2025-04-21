eac4ac57c7a723ffa6a2cbcef3ed988d198313ff
coolAbdo <abdelrahman.egschool@gmail.com>
2025-04-21 23:03:41 +0200

    refactor: Replace MongooseModule with DatabaseModule in ATSScoreModule imports

--- src/modules/ats-score/ats-score.module.ts
+++ src/modules/ats-score/ats-score.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ATSScore , ATSScoreSchema } from '../../database/schemas/ats-score.schema';
 import { ATSScoreService } from './ats-score.service';
 import { ATSScoreController } from './ats-score.controller';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: ATSScore.name, schema: ATSScoreSchema }])],
+  imports: [DatabaseModule],
   controllers: [ATSScoreController],
   providers: [ATSScoreService],
   exports: [ATSScoreService],
